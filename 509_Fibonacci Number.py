8dd6c6896b136b21d05de1488d3796cc47134886
Phong Cao <caothanhphong1105@gmail.com>
2025-08-23 16:42:53 -0400

    Implement memoization in Fibonacci calculation

--- 509_Fibonacci Number.py
+++ 509_Fibonacci Number.py
@@ -1,8 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 0:
-            return 0
+
+        # Recursion solution
+        # if n == 1:
+        #     return 1
+        # if n == 0:
+        #     return 0
+
+        # return self.fib(n-1) + self.fib(n-2)
+
+        # Memoization
+        def memoization(n, cache):
+            if n <= 1:
+                return n
             
-        return self.fib(n-1) + self.fib(n-2)
+            if n in cache:
+                return cache[n]
+
+            cache[n] = memoization(n-1, cache) + memoization(n-2, cache)
+            return cache[n]
+
+        ans = memoization(n, {})
+        return ans
