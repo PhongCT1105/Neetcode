f7ec96acf45b204d6d101c2b4b7f2cf80de89ce7
Phong Cao <caothanhphong1105@gmail.com>
2025-09-03 17:27:26 -0400

    Create 735_Asteroid Collision.py

---
+++ 735_Asteroid Collision.py
@@ -0,0 +1,18 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for i in asteroids:
+            alive = True
+
+            while alive and stack and stack[-1] > 0 and i < 0:
+                if abs(stack[-1]) < abs(i):
+                    stack.pop()
+                elif abs(stack[-1]) == abs(i):
+                    stack.pop()
+                    alive = False
+                else:
+                    alive = False
+            if alive:       
+                stack.append(i)
+                
+        return stack
