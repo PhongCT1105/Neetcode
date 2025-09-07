a831c4c89080f2d75227590c446e9e6fb34b360b
Phong Cao <caothanhphong1105@gmail.com>
2025-09-07 16:25:22 -0400

    Implement decodeString method for decoding strings

---
+++ 394_Decode String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] == "]":
+                temp = ""
+                while stack[-1] != "[":
+                    temp = stack.pop() + temp 
+                
+                stack.pop()
+
+                num = ""
+                while stack and stack[-1].isdigit():
+                    num = stack.pop() + num
+
+                stack.append(int(num) * temp)
+            else:
+                stack.append(s[i])
+
+        return "".join(stack)
