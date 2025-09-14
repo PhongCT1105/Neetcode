2f005f13d82db594bda11f3edf6596310825bd57
Phong Cao <caothanhphong1105@gmail.com>
2025-09-14 05:40:15 -0400

    Create 35_Search Insert Position.py

---
+++ 35_Search Insert Position.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+
+
+        if nums[l] < target:
+            return l + 1
+        elif nums[l] > target:
+            return l
+        else:
+            return l
